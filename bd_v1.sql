21d5323fd13e419a68466ed0870bcbcc891b7437
noobicRILLI <153820495+noobicRILLI@users.noreply.github.com>
2024-12-20 00:46:31 +0500

    Add files via upload

--- bd_v1.sql
+++ bd_v1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 18 2024 г., 22:13
+-- Время создания: Дек 19 2024 г., 22:45
 -- Версия сервера: 5.7.39
 -- Версия PHP: 8.0.22
 
@@ -24,34 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `favourite`
+--
+
+CREATE TABLE `favourite` (
+  `id` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `favourite`
+--
+
+INSERT INTO `favourite` (`id`, `id_product`, `id_user`, `count`) VALUES
+(5, 2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `order`
 --
 
 CREATE TABLE `order` (
   `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
   `id_product` int(11) NOT NULL,
-  `id_status` int(11) NOT NULL,
-  `count` int(11) NOT NULL DEFAULT '0',
-  `address` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `id_user` int(11) NOT NULL,
+  `status` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп данных таблицы `order`
---
-
-INSERT INTO `order` (`id`, `id_user`, `id_product`, `id_status`, `count`, `address`) VALUES
-(1, 3, 1, 3, 12, 'Адресс'),
-(2, 3, 1, 2, 1, 'Адрес'),
-(4, 4, 1, 3, 2, 'fasfas'),
-(8, 1, 2, 2, 2, ''),
-(9, 1, 2, 2, 2, ''),
-(10, 1, 2, 2, 1, ''),
-(11, 1, 2, 2, 2, ''),
-(12, 1, 2, 2, 0, ''),
-(13, 1, 2, 2, 0, ''),
-(14, 1, 2, 2, 0, ''),
-(15, 1, 2, 3, 0, '');
 
 -- --------------------------------------------------------
 
@@ -78,8 +79,9 @@ INSERT INTO `product` (`id`, `name`, `type`, `manufacturer`, `description`, `pri
 (2, 'MSI B760 GAMING PLUS WIFI', 'Материнская плата', 'MSI', 'ATX, сокет LGA 1700, чипсет Intel B760, 4xDDR5, 2xPCI-E 4.0, 2xM.2, сеть: 2.5 Гбит/с, Wi-Fi, Bluetooth, HDMI, DisplayPort', '16710', 'assets/img/2.webp'),
 (3, 'NVIDIA GeForce RTX 4060 MSI 8Gb (RTX 4060 VENTUS 2X BLACK 8G OC)', 'Видеокарта', 'MSI', 'PCI-E 4.0, ядро - 1830 МГц, Boost - 2505 МГц, 8 Гб памяти GDDR6 17000 МГц, 128 бит, HDMI, 3xDisplayPort, длина 199 мм, Retail', '45040', 'assets/img/3.webp'),
 (4, '2Tb SATA-III Seagate Barracuda (ST2000DM008)', 'HDD', 'Seagate', 'внутренний HDD, 3.5\", 2000 Гб, SATA-III, 7200 об/мин, кэш - 256 Мб', '8010', 'assets/img/4.webp'),
-(5, '1Tb Kingston NV2 (SNV2S/1000G)', 'SSD', 'Kingston', 'внутренний SSD, M.2, 1000 Гб, PCI-E 4.0 x4, NVMe, чтение: 3500 МБ/сек, запись: 2100 МБ/сек, 2280', '6940', 'assets/img/5.webp'),
-(6, 'Zalman N4 Rev.1 Black', 'Корпус', 'Zalman', 'корпус Midi-Tower, поддержка плат ATX, mATX, Mini-ITX, без БП, с окном, длина видеокарты до 315 мм, высота кулера до 163 мм, подсветка, 2xUSB 2.0, USB 3.0', '5930', 'assets/img/6.webp');
+(6, 'Zalman N4 Rev.1 Black', 'Корпус', 'Zalman', 'корпус Midi-Tower, поддержка плат ATX, mATX, Mini-ITX, без БП, с окном, длина видеокарты до 315 мм, высота кулера до 163 мм, подсветка, 2xUSB 2.0, USB 3.0', '5930', 'assets/img/6.webp'),
+(11, 'GeForce RTX 4060 ', '0', 'Palit', '12', '1231234', 'assets/img/400-9.webp'),
+(12, 'Стиральная машина LG F2V5GS0WT белый', '0', 'LG', 'стирка - 8.5 кг, фронтальная загрузка, отжим - 1200 об/мин, программ - 14, дозагрузка белья, пар, 54 дБ, 60 см x 85 см x 47.5 см - 53.5 см', '51999', 'assets/img/lg.webp');
 
 -- --------------------------------------------------------
 
@@ -120,14 +122,31 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`, `phone`) VALUES
-(1, 'рушан', 'zainitdinova.rozalina@yandex.ru', 'fdg', 2, 0),
-(2, 'рушан', 'zainitdinova.rozalina@yandex.ru', 'fdg', 2, 0),
-(3, 'рушан', 'catpixelpro@yandex.ru', 'fd', 2, 0),
-(10, '123', '123@12', '12', 2, 123);
+(10, '123', '123@12', '12', 2, 123),
+(11, 'рушан', 'gfd@gf', '12', 2, 12),
+(12, '111@11', '11@11', '11', 2, 11);
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `favourite`
+--
+ALTER TABLE `favourite`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `role`
@@ -146,6 +165,24 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `favourite`
+--
+ALTER TABLE `favourite`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT для таблицы `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT для таблицы `role`
 --
 ALTER TABLE `role`
@@ -155,7 +192,7 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
